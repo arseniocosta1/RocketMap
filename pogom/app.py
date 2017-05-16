3c3292e385de9d99a6af990eddfa4c21e798e568
sirlindqvist <sirlindqvist@gmail.com>
2017-05-16 23:15:00 +0200

    Revert "Merge remote-tracking branch 'origin/css' into css"
    This reverts commit 2d92cb64e83fd934ed18bc0e1316b04596a7b1bc, reversing
    changes made to 2cbca314ea38b38c9ed14d1bd75cd5bcc169631d.

--- pogom/app.py
+++ pogom/app.py
@@ -169,7 +169,6 @@ class Pogom(Flask):
             'scan_display': scan_display,
             'search_display': search_display,
             'fixed_display': not args.fixed_location
-            'custom_css': args.custom_css
         }
 
         map_lat = self.current_location[0]
--- runserver.py
+++ runserver.py
@@ -156,12 +156,13 @@ def main():
         logging.getLogger('werkzeug').setLevel(logging.DEBUG)
 
     # Check if custom.css is used otherwise fall back to default.
-    if os.path.exists(os.path.join(root_path, 'static/css/custom.css')):
-        args.custom_css = True
-        log.info('File \"custom.css\" found, applying user-defined settings.')
+    if os.path.exists(
+        os.path.join(os.path.dirname(__file__),
+                     'static/css/custom.css')):
+        log.info('custom.css found, applying user-defined settings.')
     else:
-        args.custom_css = False
-        log.info('No file \"custom.css\" found, using default settings.')
+        log.info('No custom.css found, using default settings.')
+        pass
 
     # Web access logs.
     if args.access_logs:
